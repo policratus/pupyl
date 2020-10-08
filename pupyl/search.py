80cf9db3e0eb992c76ead7e5c7975723e56f7602
policratus <policratus@gmail.com>
2020-10-08 17:36:21 -0300

    🔖 Updating to version 0.9.9

--- pupyl/search.py
+++ pupyl/search.py
@@ -5,7 +5,7 @@ Pupyl is a really fast image search library which you
 can index your own (millions of) images and find similar
 images in milliseconds.
 """
-__version__ = 'v0.9.8'
+__version__ = 'v0.9.9'
 
 
 import os
