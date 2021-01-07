e3b19e1747e7ac54a2dbf1d2a7bbb61385192266
Andrei Martins <92amartins@gmail.com>
2021-01-07 20:18:20 -0300

    🚸 Add index and serve subcommands

--- pupyl/cli.py
+++ pupyl/cli.py
@@ -8,6 +8,21 @@ class PupylCommandLineInterface:
         self.parser = argparse.ArgumentParser(
             description='Manage pupyl cli arguments.'
         )
-        self.parser.add_argument(
-            '--data_dir', help='data directory for image files'
+        self.subparsers = self.parser.add_subparsers(dest='command_name')
+
+        self.index_parser = self.subparsers.add_parser(
+            'index', help='index help'
+        )
+        self.index_parser.add_argument(
+            'input_images', help='data directory for image files'
+        )
+        self.index_parser.add_argument(
+            '--data_dir', help='data directory for assets'
+        )
+
+        self.serve_parser = self.subparsers.add_parser(
+            'serve', help='serve help'
+        )
+        self.serve_parser.add_argument(
+            '--data_dir', help='data directory for assets'
         )
