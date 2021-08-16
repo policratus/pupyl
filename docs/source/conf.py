1c806e80c8ad4de751cae89dd042b16d406c299e
policratus <policratus@gmail.com>
2021-08-16 17:37:31 -0300

    🔖 Updating to version 0.12.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,7 @@ project = '🧿 pupyl'
 copyright = f'{datetime.today().year}, Nelson Forte'
 author = 'Nelson Forte'
 
-release = '0.12.0'
+release = '0.12.1'
 version = release
 
 # -- General configuration ---------------------------------------------------
