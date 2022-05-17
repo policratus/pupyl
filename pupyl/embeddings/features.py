2523d97bdde8722e9bc8211a68f0131343b5b102
policratus <policratus@gmail.com>
2022-05-17 15:59:58 -0300

    🐛 Fixing a file resolving bug

--- pupyl/embeddings/features.py
+++ pupyl/embeddings/features.py
@@ -281,4 +281,5 @@ class Extractors(ImageIO):
         numpy.ndarray
             Tensor loaded back again.
         """
-        return numpy.load(path, allow_pickle=False, fix_imports=False)
+        if os.path.exists(path):
+            return numpy.load(path, allow_pickle=False, fix_imports=False)
