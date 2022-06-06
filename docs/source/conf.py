54ef4842a4d9a44543268fec1004d0cffe5d2c60
policratus <policratus@gmail.com>
2022-06-06 18:47:15 -0300

    🔖 Bump dependency versions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,7 @@ project = '🧿 pupyl'
 copyright = f'{datetime.today().year}, Nelson Forte'
 author = 'Nelson Forte'
 
-release = '0.13.4'
+release = '0.13.5'
 version = release
 
 # -- General configuration ---------------------------------------------------
