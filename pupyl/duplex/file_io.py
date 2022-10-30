03eee77f66f256889757e93dbec81924c7283d21
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-30 06:20:27 +0000

    Adding tarfile member sanitization to extractall()

--- pupyl/duplex/file_io.py
+++ pupyl/duplex/file_io.py
@@ -784,7 +784,26 @@ class FileIO(FileType):
             Location where to save the export file.
         """
         with tarfile.open(dump_file) as dfile:
-            dfile.extractall(output_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(dfile, output_dir)
 
     @staticmethod
     def extension(uri):
