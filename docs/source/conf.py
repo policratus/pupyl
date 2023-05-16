fd7c2681dc136ea79101832afd9f7262ecb6a022
policratus <policratus@gmail.com>
2023-05-16 18:29:15 +0100

    🔖 Bumping to v0.14.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,7 @@ project = '🧿 pupyl'
 copyright = f'{datetime.today().year}, Nelson Forte'
 author = 'Nelson Forte'
 
-release = '0.14.1'
+release = '0.14.2'
 version = release
 
 # -- General configuration ---------------------------------------------------
