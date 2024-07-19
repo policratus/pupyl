f388bcc58af3df29e488accfa8e5a06d672ebad3
policratus <policratus@gmail.com>
2024-07-19 17:53:37 -0300

    🔖 [skip ci] Bumping to version 0.14.7

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,7 @@ project = '🧿 pupyl'
 copyright = f'{datetime.today().year}, Nelson Forte'
 author = 'Nelson Forte'
 
-release = '0.14.6'
+release = '0.14.7'
 version = release
 
 # -- General configuration ---------------------------------------------------
